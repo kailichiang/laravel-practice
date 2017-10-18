2904bef2dd550feb92c6132da272281d56aad3b0
kailichiang <kelichiang@gmail.com>
2017-10-18 18:11:35 +0800

    Rendering Posts
    - Carbon, a simple PHP extension for DateTime
    - routes are ordering

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,5 +2,10 @@
 
 
 @section('content')
-    <h1>A place to show post</h1>
+    <div class="col-sm-8 blog-main">
+        <h1>{{ $post->title }}</h1>
+        <p>
+            {{ $post->body }}
+        </p>
+    </div>
 @endsection
