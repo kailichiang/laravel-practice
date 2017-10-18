dbf73b7276d8b8ab0cc6d763e3082f3c89454a2d
kailichiang <kelichiang@gmail.com>
2017-10-19 00:23:28 +0800

    Add Comments
    - php artisan make:controller CommentsController
    - Use Eloquent relationship to create an associated comment

--- app/Post.php
+++ app/Post.php
@@ -2,11 +2,23 @@
 
 namespace App;
 
+use App\Comment;
+
 class Post extends Model
 {
     public function comments()
     {
         // Comment::class equals to '\App\Comment'
         return $this->hasMany(Comment::class);
+    }
+
+    public function addComment($body)
+    {
+        // Comment::create([
+        //     'body' => $body,
+        //     'post_id' => $this->id,
+        // ]);
+
+        $this->comments()->create(compact('body'));
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -24,5 +24,29 @@
             </ul>
         </div>
 
+        {{-- Add a comment --}}
+        <hr>
+
+        <div class="card">
+            <div class="card-block">
+                <form method="POST" action="/posts/{{ $post->id }}/comments">
+                    {{-- {{ method_field('PATCH') }} --}}
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <textarea name="body" id="" placeholder="Your comment here." class="form-control" required></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add Comment</button>
+                    </div>
+
+
+                </form>
+
+                @include('layouts.errors')
+            </div>
+        </div>
+
     </div>
 @endsection
