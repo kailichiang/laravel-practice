a470f5827ea5d9c863beafb2e8c1048ff84ac668
kailichiang <kelichiang@gmail.com>
2017-10-18 17:04:10 +0800

    Create controller with a resource
    - php artisan make:controller -r TasksController

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,18 +3,87 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Task;
 
 class TasksController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
+        // GET /tasks
+
         $tasks = Task::all();
         return view('tasks.index', compact('tasks'));
     }
 
-    public function show(Task $task) // Task::find(wildcard);
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
+        // GET /tasks/create
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // POST /tasks
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        // GET /tasks/{id}
+
         return view('tasks.show', compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // GET /tasks/{id}/edit
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // PATCH /tasks/{id}
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // DELETE /tasks/{id}
     }
 }
