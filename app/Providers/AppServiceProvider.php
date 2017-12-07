e42867540b7d54a87e45404ea7db892540285f53
kailichiang <kelichiang@gmail.com>
2017-12-07 23:58:28 +0800

    Sorting Posts By Tags
    - Add tag route and controller
    - Add tags in side bar
    - Show tags in any post

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,10 @@ class AppServiceProvider extends ServiceProvider
         // \Carbon\Carbon::setLocale('zh_TW');
         // \View::composer();
         view()->composer('layouts.sidebar', function ($view) {
-            $view->with('archives', \App\Post::archives());
+            $archives = \App\Post::archives();
+            $tags = \App\Tag::has('posts')->pluck('name');
+            
+            $view->with(compact('archives', 'tags'));
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}', 'PostsController@show');
 Route::post('/posts/{post}/comments', 'CommentsController@store');
+Route::get('/posts/tags/{tag}', 'TagsController@index');
 
 Route::get('/register', 'RegistrationController@create');
 Route::post('/register', 'RegistrationController@store');
