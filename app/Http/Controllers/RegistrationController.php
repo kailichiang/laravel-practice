5b8079b6e618c649f123b356797e30343ef04010
kailichiang <kelichiang@gmail.com>
2017-12-07 12:35:12 +0800

    Sending Email
    - Generate Mailables via `php artisan make:mail Welcome`
    - Set up Mailtrap (a fake SMTP server for testing) on .env
    - Config 'mail from' via config/mail.php`
    - Pass data to mail:
      - Any public property on Mailable class
      - Any protected/private property via with() method

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Mail\Welcome;
 
 class RegistrationController extends Controller
 {
@@ -31,6 +32,8 @@ class RegistrationController extends Controller
         // Sign them in
         // \Auth::login($user); 
         auth()->login($user);
+
+        \Mail::to($user)->send(new Welcome($user));
 
         // Redirect to the home page
         return redirect()->home();
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>Welcome to Laracast! {{ $user->name }}</h1>
+    </body>
+</html>
